cc0be271443ef4b019768f391c4860f3da083655
kant300 <p.o.i.u.0987552@gmail.com>
2026-03-20 17:54:14 +0900

    chatperService

--- src/main/java/com/example/cbt/DTO/ChapterDTO.java
+++ src/main/java/com/example/cbt/DTO/ChapterDTO.java
@@ -9,15 +9,14 @@ import java.time.LocalDateTime;
 @AllArgsConstructor @NoArgsConstructor
 public class ChapterDTO {
     private Long id; //번호
-    private Integer chapterNo;//챕터번호
-    private String title;//제목
-    private String description;//설명
-    private LocalDateTime createAt;
-    private Long subjectId;//부모 테이블의 id번호
-    private String subjectName_parent;//교과목 이름
+    private Integer chapterNo; //챕터번호
+    private String title; //제목
+    private String description; //설명
+    private LocalDateTime createdAt;
+    private Long subjectId;  //교과목(부모테이블)의 id번호
+    private String subjectName; //교과목 이름
 }
-
 //DTO에는 부모/자식의 Entity를 사용
-//자식에서 부모 Entity를 사용하지 않을때는 반드시 부모의 id값을 저장
+//자식에서 부모 Entity를 사용하지 않을 때는 반드시 부모의 id값을 저장
 
 
