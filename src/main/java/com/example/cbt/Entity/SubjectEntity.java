ba10e5d7af2c53f22967033bfc084ef7b4ab9c5d
kant300 <p.o.i.u.0987552@gmail.com>
2026-03-20 15:52:17 +0900

    SubjectEntity

--- src/main/java/com/example/cbt/Entity/SubjectEntity.java
+++ src/main/java/com/example/cbt/Entity/SubjectEntity.java
@@ -6,6 +6,8 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 교과목 테이블(엔티티)
@@ -35,5 +37,15 @@ public class SubjectEntity {
     private LocalDateTime createdAt;//생성날짜
 
     //부모를 참조하는 자식테이블의 정보 및 연관관계의 제약조건
+    //하나의 교과목(1)은 여러 챕터(N)와 연관
+    //교과목이 없으면 챕터는 존재할 수가 없다. 교과목(부모), 챕터(자식)
+    //교과목은 챕터가 없어도 존재할 수있다.
+    //mappedBy 자식테이블에 선언된 부모 Entity 변수
+    //orphanRemoval = true 부모삭제시 연관된 자식 데이터도 함께 삭제
+    //orphanRemoval = false 자식이 존재시 삭제 불가능(부모삭제시 자식 데이터는 존재)
+    @OneToMany(mappedBy = "subject", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<ChapterEntity> chapters = new ArrayList<>();
+
 
 }
